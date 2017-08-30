2ba0ac720ce4455809cfe342b752eb67c8622867
KGopi21 <30748693+KGopi21@users.noreply.github.com>
2017-08-30 08:03:48 +0530

    Add files via upload

---
+++ Office365-Shell.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module MSOnline
+$UserCredential = Get-Credential tcsadmint2055@jsainsbury.onmicrosoft.com
+Connect-MsolService -Credential $UserCredential
+#For Connecting Office 365 Exchange 
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+$UserCredential = Get-Credential tcsadmint2055@jsainsbury.onmicrosoft.com
+Import-PSSession $Session
+#tcsadmint2055@jsainsbury.onmicrosoft.com
